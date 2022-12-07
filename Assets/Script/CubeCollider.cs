acacbb81497f7990275009c25bf0c4667e7915f9
kemalibu <kemalakkulak95@gmail.com>
2022-12-07 13:06:26 +0300

    I created +1 text canvas when player trigger other player cubes.

--- Assets/Script/CubeCollider.cs
+++ Assets/Script/CubeCollider.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CubeCollider : MonoBehaviour
 {
+    [SerializeField] private float destroyDelayTime = 2f;
+    [SerializeField] private float scoreTextDelay = 0.5f;
+    [SerializeField] float counter = 0;
+    [SerializeField] GameObject scoreTextCanvas;
+
     private float yOffset = 2f;
     public List<GameObject> cubes = new List<GameObject>();
-
     private GameObject lastBlock;
+
+    
 
     private void Start()
     {
         LastBlockUpdate();
+        scoreTextCanvas.SetActive(false);        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,31 +35,58 @@ public class CubeCollider : MonoBehaviour
     {
         if(other.gameObject.tag == "ObstacleCube")
         {
-            DecreasePlayerCube();
+            counter++;
+
+            if (cubes.Count > counter)
+            {
+                DecreasePlayerCube();
+            }
+
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
     void IncreasePlayerCube(GameObject gameObject)
     {
-        transform.position = new Vector3(transform.position.x,
-                             transform.position.y + yOffset,
-                             transform.position.z);
+        PositionOfCube(gameObject);
 
-        gameObject.transform.position = new Vector3(lastBlock.transform.position.x,
-                                        lastBlock.transform.position.y - yOffset,
-                                        lastBlock.transform.position.z);
+        scoreTextCanvas.SetActive(true);
+        StartCoroutine(ScoreTextChange());
         gameObject.transform.SetParent(transform);
         gameObject.transform.tag = "Player";
         cubes.Add(gameObject);
         LastBlockUpdate();
     }
 
+    void PositionOfCube(GameObject gameObject)
+    {
+        transform.position = new Vector3(transform.position.x,
+                           transform.position.y + yOffset,
+                           transform.position.z);
+
+        gameObject.transform.position = new Vector3(lastBlock.transform.position.x,
+                                        lastBlock.transform.position.y - yOffset,
+                                        lastBlock.transform.position.z);
+    }
+
+    IEnumerator ScoreTextChange()
+    {
+        yield return new WaitForSeconds(scoreTextDelay);
+        scoreTextCanvas.SetActive(false);
+    }
+
     void DecreasePlayerCube()
     {
         lastBlock.transform.parent = null;
         cubes.Remove(lastBlock);
+        Destroy(lastBlock, destroyDelayTime);
         LastBlockUpdate();
+        counter = 0;
     }
+
 
     void LastBlockUpdate()
     {
